055294c5d071f172bae75b7a0a5caca2bf090f20
ohsangsoo <tkdtn3264@gmail.com>
2023-06-20 17:17:52 +0900

    아이템 리스트 만들기

--- lib/Item.dart
+++ lib/Item.dart
@@ -2,6 +2,15 @@ class Item{
     final String title; // 상품이름
     final int price;// 가격
 
-    Item(this.title, this.price);
+    // 생성자 만들기
+  Item(this.title, this.price);
 
 }
+//  아이템 리스트 만들기
+final itemList = [
+  Item("맥북에어" , 2000000),
+  Item("맥북프로" , 3500000),
+  Item("아이폰14프로" ,1550000),
+  Item("아이폰14프로맥스",1900000),
+  Item("아이맥" , 3000000),
+];
