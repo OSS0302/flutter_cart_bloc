4a40540a3c57836ed96ee73e9caa70516e1e30be
ohsangsoo <tkdtn3264@gmail.com>
2023-06-21 18:11:33 +0900

    @override 해서 두가지를 추가

--- lib/bloc/cart_bloc.dart
+++ lib/bloc/cart_bloc.dart
@@ -1,1 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../Item.dart';
+
+enum CartEventType{
+  add, remove
+}
+class CartEvent{
+  final CartEventType type;
+  final Item item;
+
+  // 생성자 생성
+  CartEvent(this.type, this.item);
+}
+class CartBloc extends Bloc<CartEvent ,List<Item>>{
+@override
+  //TODO: implement initialState
+  List<Item> get initialState => null;
+@override
+  Stream<List<Item>> mapEventToState(CartEvent event){
+  //TODO: implements mapEventToState
+  return null;
+  }
+}
