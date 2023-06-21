8219af219f36ffc98f803217308d0672540c41b5
ohsangsoo <tkdtn3264@gmail.com>
2023-06-21 21:41:41 +0900

    현재 상태를 스트림을 통해서 추가 와 삭제 이벤트가 있을 때마다 밀어낸다.

--- lib/bloc/cart_bloc.dart
+++ lib/bloc/cart_bloc.dart
@@ -1,3 +1,6 @@
+
+import 'package:bloc/bloc.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../Item.dart';
@@ -12,13 +15,25 @@ class CartEvent{
   // 생성자 생성
   CartEvent(this.type, this.item);
 }
-class CartBloc extends Bloc<CartEvent ,List<Item>>{
-@override
-  //TODO: implement initialState
-  List<Item> get initialState => null;
-@override
-  Stream<List<Item>> mapEventToState(CartEvent event){
-  //TODO: implements mapEventToState
-  return null;
+class CartBloc extends Bloc<CartEvent, List<Item>>{
+  CartBloc(super.initialState);
+
+  @override
+  List<Item>? get initialState => []; //initialState 초기값
+
+  get currentState => null;
+
+  @override
+  Stream<List<Item>> mapEventToState(CartEvent , event) async*{
+    switch(event.type){
+      case CartEventType.add:
+        currentState.add(event.item);
+        break;
+      case CartEventType.remove:
+        currentState.remove(event.item);
+        break;
+    }
+    yield* currentState;
+
   }
 }
