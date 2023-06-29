03acc7a0f9f23705ac4920d28153d7e1a8dc1c80
ohsangsoo <tkdtn3264@gmail.com>
2023-06-29 14:21:28 +0900

    카트 블럭을 받는다.

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,4 +1,7 @@
  import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cart_bloc/bloc/cart_bloc.dart';
+
 
 class Cart extends StatefulWidget {
   const Cart({Key? key}) : super(key: key);
@@ -11,10 +14,12 @@ class _CartState extends State<Cart> {
 
   @override
   Widget build(BuildContext context) {
+    final _cartBloc = BlocProvider.of<CartBloc>(context);
     return Scaffold(
       appBar:  AppBar(
         title: Text('Cart'),
       ),
     );
   }
+
 }
