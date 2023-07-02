7792e07a584cb014d8de696788ec04fb026176a2
ohsangsoo <tkdtn3264@gmail.com>
2023-07-02 15:55:21 +0900

    builder provider 지우고 stream 변경하기

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cart_bloc/bloc/cart_bloc.dart';
 import 'package:flutter_cart_bloc/Item.dart';
 import 'package:flutter_cart_bloc/cart.dart';
+import 'package:flutter_cart_bloc/main.dart';
 
 // 두번째 화면 stful 위젯
 class Catalog extends StatefulWidget {
@@ -13,11 +14,9 @@ class Catalog extends StatefulWidget {
 }
 
 class _CatalogState extends State<Catalog> {
-  List<Item> _itemList = itemList;
 
   @override
   Widget build(BuildContext context) {
-    final _cartBloc = BlocProvider.of<CartBloc>(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -32,23 +31,24 @@ class _CatalogState extends State<Catalog> {
           ),
         ],
       ),
-      body: BlocProvider(
-        create: (BuildContext context) => _cartBloc,
-        child: BlocBuilder<CartBloc, CartState>(
-            bloc: _cartBloc,
-            builder: (context, state) {
-              return ListView(
-                children: _itemList
-                    .map((item) => _buildItem(item, state.cartList!, _cartBloc))
-                    .toList(), //리스트로 재변환
-              );
-            }),
-      ),
+      body: StreamBuilder(
+        stream: cartBloc.cartList,
+          builder: (context, snapshot){
+          print('hihih' );
+          return snapshot.data != null
+              ? ListView(
+            children: cartBloc.itemList
+                .map((item) => _buildItem(item, snapshot.data!))
+                .toList(), //리스트로 재변환
+          ) : Container();
+          }
+      )
     );
   }
 
-  Widget _buildItem(Item item, List state, CartBloc cartBloc) {
+  Widget _buildItem(Item item, List<Item> state ) {
     final isChecked = state.contains(item); // 아이템 있는지 확인한다.
+    // final isChecked = state.contains(item); // 아이템 있는지 확인한다.
     return Padding(
       child: ListTile(
         title: Text(
