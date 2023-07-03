36f63c3df69b8b998e7c54535357fc5f42bf20d2
ohsangsoo <tkdtn3264@gmail.com>
2023-07-03 16:04:30 +0900

    아이템 만들어서 cartbloc에 던진다.

--- lib/catalog.dart
+++ lib/catalog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cart_bloc/bloc/cart_bloc.dart';
 import 'package:flutter_cart_bloc/Item.dart';
+import 'package:flutter_cart_bloc/bloc/cart_provider.dart';
 import 'package:flutter_cart_bloc/cart.dart';
 import 'package:flutter_cart_bloc/main.dart';
 
@@ -17,7 +17,7 @@ class _CatalogState extends State<Catalog> {
 
   @override
   Widget build(BuildContext context) {
-
+    CartBloc cartBloc = CartProvider.of(context); // cartProvider에서 만든 context 카트블럭을 를가져와서 cartBloc애 넣는다.
     return Scaffold(
       appBar: AppBar(
         title: Text('Catalog'),
@@ -38,7 +38,7 @@ class _CatalogState extends State<Catalog> {
           return snapshot.data != null
               ? ListView(
             children: cartBloc.itemList
-                .map((item) => _buildItem(item, snapshot.data!))
+                .map((item) => _buildItem(item, snapshot.data!, cartBloc))
                 .toList(), //리스트로 재변환
           ) : Container();
           }
@@ -46,7 +46,7 @@ class _CatalogState extends State<Catalog> {
     );
   }
 
-  Widget _buildItem(Item item, List<Item> state ) {
+  Widget _buildItem(Item item, List<Item> state, CartBloc cartBloc ) {
     final isChecked = state.contains(item); // 아이템 있는지 확인한다.
     // final isChecked = state.contains(item); // 아이템 있는지 확인한다.
     return Padding(
