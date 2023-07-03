6df0f48b5208fa5cc6b0e879c2644e4e5fb4ce4d
ohsangsoo <tkdtn3264@gmail.com>
2023-07-03 15:53:41 +0900

    Cartbloc을 지정을 안하면 cartProvider만든 로직을 수행한다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cart_bloc/bloc/cart_bloc.dart';
+import 'package:flutter_cart_bloc/bloc/cart_provider.dart';
 import 'package:flutter_cart_bloc/catalog.dart';
 
 void main() {
   runApp(MyApp());
-  final cartBloc = CartBloc(); // bloc 최상단에 선언하고 했었지만 provider 패턴을 내림 선언 하는걿 선호 해서 지운다.
+
 }
 
   final cartBloc = CartBloc();
 class MyApp extends StatelessWidget {
-
   MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CartProvider(
+      child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.lightBlue,
       ),
       home: LoginScreen(),
+    ),
     );
   }
 }
